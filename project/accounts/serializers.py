aa8fc3d3d741f08c4d9c0382f30a0be0073a4db8
John <abreganajohnmark@gmail.com>
2023-04-26 22:31:32 +0800

    Updated authorization

--- project/accounts/serializers.py
+++ project/accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from djoser.serializers import UserCreateSerializer
+from djoser.serializers import UserCreateSerializer,UserSerializer
 from djoser.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
@@ -9,6 +9,17 @@ from accounts.models import Profile
 
 
 User = get_user_model()
+
+class CustomUserSerializer(UserSerializer):
+    class Meta:
+        model = User
+        fields = tuple(User.REQUIRED_FIELDS) + (
+            settings.USER_ID_FIELD,
+            settings.LOGIN_FIELD,
+            'first_name',
+            'last_name'
+        )
+        read_only_fields = (settings.LOGIN_FIELD,)
 
 class CustomUserCreateSerializer(UserCreateSerializer):
     first_name = serializers.CharField(max_length = 225, write_only=True)
